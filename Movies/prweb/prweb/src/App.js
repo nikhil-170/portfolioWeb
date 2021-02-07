124675f0abc11486308e86db07b220ce02ecf153
nikhil-170 <nikhil.peteti@gmail>
2021-02-07 13:19:57 -0500

    Navbar completed

--- Movies/prweb/prweb/src/App.js
+++ Movies/prweb/prweb/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/navbar.js";
 function App() {
   return (
-    <h2>Portfolio project</h2>
+    <Navbar />
   );
 }
 
