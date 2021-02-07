5e15a223f63408304fc436491b6988c6e24fe7ce
nikhil-170 <nikhil.peteti@gmail>
2021-02-07 18:06:25 -0500

    Header Complete

--- Movies/prweb/prweb/src/App.js
+++ Movies/prweb/prweb/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./Components/navbar.js";
+import Header from "./Components/Header";
 function App() {
   return (
-    <Navbar />
+    <>
+      <Particles
+      className="particles-canvas"
+      params = {{
+        particles: {
+          number:{
+            value: 30,
+            density: {
+              enable: true,
+              value_area : 900
+            }
+          },
+          shape: {
+            type: "square",
+            stroke: {
+              width: 6,
+              color: "#7df9ff"
+            }
+          }
+        }
+      }
+
+      }
+      />
+      <Navbar />
+      <Header/>
+    </>
   );
 }
 
